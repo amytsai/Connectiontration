d1d195eec786f55af1a7ef2bf2a5756241279729
Amy <amy.tsai@berkeley.edu>
2014-07-26 23:22:39 -0700

    cleanup sidebar

--- public/js/main.js
+++ public/js/main.js
@@ -186,14 +186,14 @@ Game = {
       var oldCard = this.cards[this.parseId(this.oldSelected)];
       if(newCard.name === oldCard.name) {
         Render.sidebarSuccess();
-        $(document).bind("click", $.proxy(function() {
+        /*$(document).bind("click", $.proxy(function() {
           this.successfulMatch(this.oldSelected, cardEl);
-        }, this));
+        }, this));*/
       } else {
         Render.sidebarFail()
-        $(document).bind("click", $.proxy(function() {
+        /*$(document).bind("click", $.proxy(function() {
           this.failedMatch(this.oldSelected, cardEl);
-        }, this));
+        }, this));*/
       }
     } else {
       this.oldSelected = cardEl;
