ba1029aae9b04ddd331d981860dee51cded92a69
Amy <amy.tsai@berkeley.edu>
2014-07-26 20:44:31 -0700

    bug fix

--- public/js/main.js
+++ public/js/main.js
@@ -139,7 +139,7 @@ Game = {
   shuffle: function(o) { 
       for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
       return o;
-  }
+  },
 
   selectCard: function(cardEl) {
     cardEl.style.visibility = "visible";
