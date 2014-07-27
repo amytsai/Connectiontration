f914a2a626637e7fe9a2d34180ff1759fa3ce1e0
Amy <amy.tsai@berkeley.edu>
2014-07-26 23:02:32 -0700

    added sidebar logic

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,8 @@ Render =  {
   SELECTORS: {
     loginButton: "#loginButton",
     overlay: ".overlay",
-    game: "#table"
+    game: "#table",
+    sidebar: "#sidebar"
   },
 
   loginButton: function() {
@@ -45,7 +46,7 @@ Render =  {
     cardEl.id = "card-" + n;
 
     var cardBack = document.createElement("div");
-    cardBack.className = "card-back"
+    cardBack.className = "card-back";
     cardEl.appendChild(cardBack);
     if(card.isPicture) {
       imgEl = document.createElement("img");
@@ -55,6 +56,33 @@ Render =  {
       $(cardBack).html(card.name);
     }
     return cardEl;
+  },
+
+  sidebarAdd: function(card) {
+    var newEl = document.createElement("div");
+    newEl.className = "card-desc";
+    if(card.isPicture) {
+      imgEl = document.createElement("img");
+      imgEl.src = card.pictureUrl;
+      $(newEl).append(imgEl);
+    } else {
+      $(newEl).html(card.name);
+    }
+    $(this.SELECTORS.sidebar).append(newEl);
+  },
+
+  sidebarSuccess: function() {
+    var newEl = document.createElement("div");
+    newEl.className = "success";
+    newEl.html("Its a Match!");
+    $(this.SELECTORS.sidebar).append(newEl)
+  },
+
+  sidebarFail: function() {
+    var newEl = document.createElement("div");
+    newEl.className = "fail";
+    newEl.html("Not a Match");
+    $(this.SELECTORS.sidebar).append(newEl);
   }
 
 }
@@ -147,14 +175,21 @@ Game = {
     var id = this.parseId(cardEl);
     var newCard = this.cards[id];
 
+    Render.sidebarAdd(newCard);
     if(this.oldSelected && this.oldSelected != cardEl) {
+      //unbind card click events for next click
+      $(".card").unbind("click");
       var oldCard = this.cards[this.parseId(this.oldSelected)];
       if(newCard.name === oldCard.name) {
-        this.successfulMatch(this.oldSelected, cardEl);
-        this.oldSelected = null;
+        Render.sidebarSuccess();
+        $(document).bind("click", function() {
+          this.successfulMatch(this.oldSelected, cardEl);
+        });
       } else {
-        this.failedMatch(this.oldSelected, cardEl);
-        this.oldSelected = null;
+        Render.sidebarFail()
+        $(document).bind("click", function() {
+          this.failedMatch(this.oldSelected, cardEl);
+        });
       }
     } else {
       this.oldSelected = cardEl;
@@ -166,11 +201,19 @@ Game = {
     $(card2).empty();
     $(card1).addClass('cleared');
     $(card2).addClass('cleared');
+    this.oldSelected = null;
+    $(".card").bind("click", function() {
+      Game.selectCard(this);
+    })
   },
 
   failedMatch: function(card1, card2) {
     card1.firstChild.style.visibility = "hidden";
     card2.firstChild.style.visibility = "hidden";
+    this.oldSelected = null;
+    $(".card").bind("click", function() {
+      Game.selectCard(this);
+    })
   },
 
   parseId: function(cardEl) {
